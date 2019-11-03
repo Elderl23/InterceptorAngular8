340c3e45d49fb4ad645081690dc156b1678d9280
Elder <lopez_felix_23@hotmail.com>
2019-11-03 13:26:07 -0600

    Initi commit

---
+++ src/app/headers.interceptor.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class HeadersInterceptor implements HttpInterceptor {
+    constructor() { }
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        let ContentType = 'application/json; charset=utf-8';
+        let authorization = '';
+
+        if (sessionStorage.getItem('currentUser') !== null) {
+            authorization = `Token  ${sessionStorage.getItem('currentUser').replace(/['"]+/g, '')}`;
+        }
+
+        request = request.clone({
+            setHeaders: {
+                Authorization: authorization,
+                "Content-Type": ContentType,
+            }
+        });
+        return next.handle(request);
+    }
+}
